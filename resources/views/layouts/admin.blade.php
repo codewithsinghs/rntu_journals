2f6a73ccf0f47ba6fc01f1fa12042495e4a4ff78
codewithsinghs <amresh@bpl1.com>
2026-08-08 12:12:56 +0530

    scale added to layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
 <head>
 
     <title>RNTU Journal</title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Boostrp 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
