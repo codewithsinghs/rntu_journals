1bf0882a7044ec0fa1726759e5b7a7018b3e5a28
UnnatiPathak <unnatipathak78@gmail.com>
2026-08-13 11:04:53 +0530

    changes of hiding articles on frontend which is deleted

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -74,6 +74,8 @@ class HomeController extends Controller
             $approvedArticles = DB::table('submit_articles as sa')
                 ->join('article_reviews as ar', 'ar.submit_article_id', '=', 'sa.id')
                 ->where('ar.editor_status', 'approved')
+                ->where('sa.deleted_at', null)
+                ->where('sa.is_hidden', false)
                 ->orderByDesc('ar.updated_at')
                 ->select(
                     'sa.id',
